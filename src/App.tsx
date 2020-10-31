e468e640fdbf27f39277b4a260d4237cc10324f0
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2020-10-31 10:05:17 +0000

    style: add background and middle stripe

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
 import React from "react";
-
 import Header from "./components/Header";
 import Questions from "./components/Questions";
 
+const style = {
+  background: {
+    height: "100vh",
+    backgroundColor: "rgb(245, 245, 245)",
+  },
+  middleStripe: {
+    height: "100vh",
+    backgroundColor: "white",
+    maxWidth: 500,
+    margin: "0 auto",
+  },
+};
+
 function App() {
   return (
-    <div style={{ backgroundColor: "rgb(245, 245, 245)" }}>
-      <div style={{ maxWidth: 500, margin: "0 auto" }}>
+    <div style={style.background}>
+      <div style={style.middleStripe}>
         <Header />
         <Questions />
       </div>
