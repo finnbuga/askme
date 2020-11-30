55adcf58811bc35e7ce7e75626b2497331e50045
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2020-11-30 22:26:51 +0000

    refactor: use then / catch to return promisses from API calls

--- src/api/questions.ts
+++ src/api/questions.ts
@@ -5,19 +5,7 @@ import firebase from "./firebase"
 
 const questionsRef = firebase.firestore().collection("questions")
 
-export const getQuestions = async () => {
-  try {
-    const querySnapshot = await questionsRef.get()
-    const fetchedQuestions: Question[] = []
-    querySnapshot.forEach((doc) => {
-      fetchedQuestions.push({ id: doc.id, text: doc.data().text })
-    })
-    return fetchedQuestions
-  } catch (error) {
-    return Promise.reject(error)
-  }
-}
+export const getQuestions = () =>
+  questionsRef.get().then(({ docs }) => docs.map((doc) => ({ id: doc.id, text: doc.data().text })))
 
-export const deleteQuestion = async (id: Question["id"]) => {
-  questionsRef.doc(id).delete()
-}
+export const deleteQuestion = (id: Question["id"]) => questionsRef.doc(id).delete()
