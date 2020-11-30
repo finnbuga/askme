0e8b16e41d4eea17bc0cb941e873f5d5f6ff985f
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2020-11-30 23:01:23 +0000

    refactor: export functions that dispatch actions rather than action creators

--- src/components/useQuestions.ts
+++ src/components/useQuestions.ts
@@ -1,13 +1,13 @@
 import { useEffect } from "react"
 
-import { useSelector, dispatch } from "store"
+import { useSelector } from "store"
 import { getQuestions } from "store/questionsSlice"
 
 const useQuestions = () => {
   const questions = useSelector((state) => state.questions)
 
   useEffect(() => {
-    dispatch(getQuestions())
+    getQuestions()
   }, [])
 
   return questions
