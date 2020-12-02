e716059bb09688c340c0885b57b39474589fe171
Finn Buga <florin.buga@getground.co.uk>
2020-12-02 08:49:03 +0000

    feat: move error and isLoading state to local and display loader

--- src/components/useQuestions.ts
+++ src/components/useQuestions.ts
@@ -1,16 +1,23 @@
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 
 import { useSelector } from "store"
 import { getQuestions } from "store/questionsSlice"
 
 const useQuestions = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [error, setError] = useState<string | null>(null)
   const questions = useSelector((state) => state.questions)
 
   useEffect(() => {
-    getQuestions()
+    setIsLoading(true)
+    getQuestions().then(({ payload }) => {
+      // @todo use the error message in payload.error
+      setError(payload ? null : "Failed fetching questions")
+      setIsLoading(false)
+    })
   }, [])
 
-  return questions
+  return { isLoading, error, questions }
 }
 
 export default useQuestions
