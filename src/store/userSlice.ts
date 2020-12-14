d92c6886a084a6515345251a2b517ac0f5bfe2b8
Finn Buga <florin.buga@getground.co.uk>
2020-12-14 22:28:30 +0000

    feat: add thunk action creators for adding / removing liked questions

--- src/store/userSlice.ts
+++ src/store/userSlice.ts
@@ -1,12 +1,51 @@
-import { createSlice } from "@reduxjs/toolkit"
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit"
 
 import User from "api/interfaces/User"
+import Question from "api/interfaces/Question"
+import * as api from "api/users"
+import { dispatch, RootState } from "store"
+
+const addLikedQuestion = createAsyncThunk<Promise<any>, Question["id"], { state: RootState }>(
+  "questions/addLikedQuestion",
+  async (questionId: string, { getState }) => api.addLikedQuestion(getState().user!.id, questionId)
+)
+// @todo use connect?
+const dispatchAddLikedQuestion = (id: Question["id"]) => dispatch(addLikedQuestion(id))
+export { dispatchAddLikedQuestion as addLikedQuestion }
+
+const removeLikedQuestion = createAsyncThunk<Promise<any>, Question["id"], { state: RootState }>(
+  "questions/removeLikedQuestion",
+  async (questionId: string, { getState }) =>
+    api.removeLikedQuestion(getState().user!.id, questionId)
+)
+const dispatchRemoveLikedQuestion = (id: Question["id"]) => dispatch(removeLikedQuestion(id))
+export { dispatchRemoveLikedQuestion as removeLikedQuestion }
 
 const userSlice = createSlice({
   name: "user",
+
   initialState: null as User | null,
+
   reducers: {
     setUser: (_, action) => action.payload,
+  },
+
+  extraReducers: (builder) => {
+    builder.addCase(addLikedQuestion.fulfilled, (state, action) => {
+      const questionId = action.meta.arg
+      if (!state!.likedQuestions) {
+        // @todo use Set()
+        state!.likedQuestions = []
+      }
+      state!.likedQuestions.push(questionId)
+    })
+
+    builder.addCase(removeLikedQuestion.fulfilled, (state, action) => {
+      const removedQuestionId = action.meta.arg
+      state!.likedQuestions = state!.likedQuestions!.filter(
+        (question) => question !== removedQuestionId
+      )
+    })
   },
 })
 
