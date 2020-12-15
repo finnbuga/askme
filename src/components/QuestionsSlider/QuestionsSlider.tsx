8a6a0a151594c0f2e1f281e9fed99dad09036fc8
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2020-12-15 08:42:21 +0000

    refactor: QuestionsSlider

--- src/components/QuestionsSlider/QuestionsSlider.tsx
+++ src/components/QuestionsSlider/QuestionsSlider.tsx
@@ -8,13 +8,13 @@ import useLike from "./useLike"
 
 const QuestionsSlider: React.FC<{ questions: Question[] }> = ({ questions }) => {
   const { current, goToNext, goToPrev } = useNavigator(questions.length)
-  const currentSlideText = questions.length ? questions[current].text : "No questions"
+  const currentQuestion = questions[current]
 
-  const { isLiked, like } = useLike(questions[current].id)
+  const { isLiked, like } = useLike(currentQuestion?.id)
 
   return (
     <>
-      <BigTextCard>{currentSlideText}</BigTextCard>
+      <BigTextCard>{questions.length ? currentQuestion.text : "No questions"}</BigTextCard>
 
       <ControlButtons goToNext={goToNext} goToPrev={goToPrev} like={like} isLiked={isLiked} />
     </>
