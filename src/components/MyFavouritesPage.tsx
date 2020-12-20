772582f5564dc951bba9ee89d59618c47c172fc2
Finn Buga <florin.buga@getground.co.uk>
2020-12-20 01:04:36 +0000

    feat: implement My Favourites page

--- src/components/MyFavouritesPage.tsx
+++ src/components/MyFavouritesPage.tsx
@@ -1,8 +1,24 @@
 import React from "react"
 import { RouteComponentProps } from "@reach/router"
 
+import { useSelector } from "store"
+import useQuestions from "./useQuestions"
+import QuestionsSlider from "components/QuestionsSlider"
+
 const MyFavouritesPage: React.FC<RouteComponentProps> = () => {
-  return <div>Placeholder</div>
+  const user = useSelector((state) => state.user)
+  const { questions, isLoading, error } = useQuestions()
+  const myFavourites = questions?.filter((question) => user?.likedQuestions?.includes(question.id))
+
+  if (isLoading) {
+    return <div>Loading...</div>
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>
+  }
+
+  return <QuestionsSlider questions={myFavourites!} />
 }
 
 export default MyFavouritesPage
