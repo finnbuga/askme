4c3e101c6b1809a2357a70054989a9db9af9e8a2
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2021-08-08 12:09:43 +0100

    feat: add bottom navigation

---
+++ src/App/Navigation.tsx
@@ -0,0 +1,19 @@
+import * as React from "react"
+import { Paper, BottomNavigation, BottomNavigationAction } from "@material-ui/core"
+import { Favorite, Person } from "@material-ui/icons"
+import { useState } from "react"
+
+const Navigation: React.FC = () => {
+  const [value, setValue] = useState(0)
+
+  return (
+    <Paper sx={{ position: "absolute", bottom: 0, left: 0, right: 0 }} elevation={3}>
+      <BottomNavigation showLabels value={value} onChange={(_, newValue) => setValue(newValue)}>
+        <BottomNavigationAction label="Favorites" icon={<Favorite />} />
+        <BottomNavigationAction label="My Questions" icon={<Person />} />
+      </BottomNavigation>
+    </Paper>
+  )
+}
+
+export default Navigation
