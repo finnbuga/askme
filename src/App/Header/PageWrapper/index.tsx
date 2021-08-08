982e2bac85e664fa80458552c66a3f718f1350db
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2021-08-08 13:20:04 +0100

    refactor: style

---
+++ src/App/Header/PageWrapper/index.tsx
@@ -0,0 +1,22 @@
+import React from "react"
+import { Box, Card, Container, Theme } from "@material-ui/core"
+
+import backgroundImage from "./background.jpg"
+
+const wrapperStyle = {
+  height: "100vh",
+  background: (theme: Theme) => ({
+    xs: theme.palette.grey[200],
+    sm: `url(${backgroundImage}) 0% 0% / cover`,
+  }),
+}
+
+const PageWrapper: React.FC = ({ children }) => (
+  <Box sx={wrapperStyle}>
+    <Container component={Card} maxWidth="sm" disableGutters>
+      {children}
+    </Container>
+  </Box>
+)
+
+export default PageWrapper
--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,40 +1,41 @@
 import React from "react"
 import { Router } from "@reach/router"
 import { createTheme, ThemeProvider } from "@material-ui/core/styles"
-import { Card, Container, Theme } from "@material-ui/core"
+import { CssBaseline, Theme } from "@material-ui/core"
+import teal from "@material-ui/core/colors/teal"
 
 import "./getUser"
-import Header from "App/Header"
 import HomePage from "pages/HomePage"
 import MyQuestionsPage from "pages/MyQuestionsPage"
 import MyFavouritesPage from "pages/MyFavouritesPage"
+import Header from "./Header"
+import PageWrapper from "./Header/PageWrapper"
 
 declare module "@material-ui/styles" {
   interface DefaultTheme extends Theme {}
 }
 
-const theme = createTheme()
-
-const style = {
-  triplePadding: {
-    padding: 24,
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: teal[500],
+    },
   },
-}
+  spacing: 8,
+})
 
 function App() {
   return (
     <ThemeProvider theme={theme}>
-      <Container maxWidth="sm">
-        <Card>
-          <Header />
-
-          <Router component="main" style={style.triplePadding}>
-            <MyQuestionsPage path="my-questions" />
-            <MyFavouritesPage path="my-favourites" />
-            <HomePage path="*" />
-          </Router>
-        </Card>
-      </Container>
+      <CssBaseline />
+      <PageWrapper>
+        <Header />
+        <Router component="main" style={{ padding: 24 }}>
+          <MyQuestionsPage path="my-questions" />
+          <MyFavouritesPage path="my-favourites" />
+          <HomePage path="*" />
+        </Router>
+      </PageWrapper>
     </ThemeProvider>
   )
 }
