0871ad4875b555a07437e919b3f29563cfc90226
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2021-08-10 22:50:10 +0100

    fix: use dispatch directly when applying .then()

--- src/hooks/useQuestions.ts
+++ src/hooks/useQuestions.ts
@@ -1,28 +1,29 @@
 import { useEffect, useState } from "react"
 
 import Question from "api/interfaces/Question"
-import { useSelector } from "store"
+import { useDispatch, useSelector } from "store"
 import { questionsActions } from "store/questionsSlice"
-import useDispatchActions from "store/useDispatchActions"
 
 const useQuestions = (filter?: (question: Question) => boolean) => {
-  const { getQuestions } = useDispatchActions(questionsActions)
-  const [state, setState] = useState({
-    isLoading: true,
-    error: null as string | null,
-  })
-
   let questions = useSelector((state) => state.questions)
   if (filter) {
     questions = questions.filter(filter)
   }
 
+  const [state, setState] = useState({
+    isLoading: true,
+    error: null as string | null,
+  })
+
+  const dispatch = useDispatch()
+
   useEffect(() => {
-    getQuestions().then(({ payload }) => {
+    const { getQuestions } = questionsActions
+    dispatch(getQuestions()).then(({ payload }) => {
       // TODO use the error message in payload.error
       setState({ isLoading: false, error: payload ? null : "Failed fetching questions" })
     })
-  }, [getQuestions])
+  }, [dispatch])
 
   return { questions, ...state }
 }
