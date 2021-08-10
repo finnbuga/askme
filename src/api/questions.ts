277f34b467fb28c0782391f7ba3e21886ecb56a0
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2021-08-10 22:46:11 +0100

    fix: type api response

--- src/api/questions.ts
+++ src/api/questions.ts
@@ -9,10 +9,12 @@ export const addQuestion = (question: Omit<Question, "id">) =>
   questionsRef.add(question).then((questionRef) => ({ id: questionRef.id, ...question }))
 
 export const getQuestions = () =>
-  questionsRef
-    .get()
-    .then(({ docs }) =>
-      docs.map((doc) => ({ id: doc.id, text: doc.data().text, userId: doc.data().userId }))
-    )
+  questionsRef.get().then(({ docs }) =>
+    docs.map((doc) => ({
+      id: doc.id,
+      text: doc.data().text as string,
+      userId: doc.data().userId as string,
+    }))
+  )
 
 export const deleteQuestion = (id: Question["id"]) => questionsRef.doc(id).delete()
