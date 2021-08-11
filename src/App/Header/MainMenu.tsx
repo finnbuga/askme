0ac22c70c99bdbc09a9a923324ae611a14f72695
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2021-08-11 22:23:05 +0100

    feat: enhance user menu

--- src/App/Header/MainMenu.tsx
+++ src/App/Header/MainMenu.tsx
@@ -2,8 +2,8 @@ import React, { useRef } from "react"
 import { navigate } from "@reach/router"
 import { IconButton, Menu, MenuItem, ListItemIcon, ListItemText } from "@material-ui/core"
 import MenuIcon from "@material-ui/icons/Menu"
-import FavoriteIcon from "@material-ui/icons/Favorite"
-import FolderIcon from "@material-ui/icons/Folder"
+import FavoriteIcon from "@material-ui/icons/FavoriteBorder"
+import FolderIcon from "@material-ui/icons/FolderOpen"
 
 import useToggle from "hooks/useToggle"
 
--- src/App/Header/UserMenu.tsx
+++ src/App/Header/UserMenu.tsx
@@ -1,25 +1,45 @@
-import React from "react"
+import React, { useRef } from "react"
 import { useSelector } from "store"
 import { navigate } from "@reach/router"
-import { Button } from "@material-ui/core"
+import { Button, IconButton, Menu, MenuItem, ListItemIcon, ListItemText } from "@material-ui/core"
+import AccountCircle from "@material-ui/icons/AccountCircle"
+import ExitToAppIcon from "@material-ui/icons/ExitToApp"
 
 import { signInWithGoogle, signOut } from "api/auth"
+import useToggle from "hooks/useToggle"
 
 function UserMenu() {
   const { user, isAuthenticating } = useSelector((state) => state.auth)
-  const isLoggedIn = Boolean(user)
+  const menuButtonRef = useRef(null)
+  const [isOpen, openMenu, closeMenu] = useToggle(false)
 
-  return isAuthenticating ? null : isLoggedIn ? (
-    <>
-      {user!.name} -
-      <Button color="inherit" onClick={() => navigate("/").then(signOut)}>
-        Log out
+  if (isAuthenticating) {
+    return null
+  }
+
+  if (!user) {
+    return (
+      <Button color="inherit" onClick={signInWithGoogle}>
+        Login
       </Button>
+    )
+  }
+
+  return (
+    <>
+      <IconButton edge="start" color="inherit" onClick={openMenu} ref={menuButtonRef}>
+        <AccountCircle />
+      </IconButton>
+
+      <Menu open={isOpen} onClose={closeMenu} anchorEl={menuButtonRef.current} keepMounted>
+        <MenuItem onClick={() => navigate("/").then(signOut)}>
+          <ListItemIcon>
+            <ExitToAppIcon />
+          </ListItemIcon>
+          <ListItemText>Logout</ListItemText>
+        </MenuItem>
+      </Menu>
     </>
-  ) : (
-    <Button color="inherit" onClick={signInWithGoogle}>
-      Login
-    </Button>
   )
 }
 
