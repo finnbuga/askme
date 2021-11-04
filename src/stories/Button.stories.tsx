4708d2c5458f06dd5f071fc2ca64c3bc4bb799f1
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2021-11-04 23:16:41 +0000

    refactor: use component decorator

--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -6,10 +6,19 @@ import { Button, ButtonProps } from "@material-ui/core"
 export default {
   title: "Button",
   component: Button,
+  decorators: [
+    (Story) => (
+      <div
+        style={{ display: "flex", flexDirection: "column", gap: 24, maxWidth: 200, margin: "auto" }}
+      >
+        <Story />
+      </div>
+    ),
+  ],
 } as Meta
 
 export const ButtonTypes: Story<ButtonProps> = ({ color, onClick }) => (
-  <div style={{ display: "flex", flexDirection: "column", gap: 24, maxWidth: 200, margin: "auto" }}>
+  <>
     <Button color="secondary" onClick={onClick}>
       Secondary
     </Button>
@@ -25,7 +34,7 @@ export const ButtonTypes: Story<ButtonProps> = ({ color, onClick }) => (
     <Button color={color} variant="contained" size="small" onClick={onClick}>
       Small
     </Button>
-  </div>
+  </>
 )
 ButtonTypes.args = {
   color: "primary",
