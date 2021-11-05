3594eb1e820b6f17eca4c0a5198ccfda66431656
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2021-11-05 21:00:53 +0000

    feat: add Mui theme as global decorator in Storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,4 +1,18 @@
+import { ThemeProvider } from "@material-ui/core/styles"
+import { CssBaseline } from "@material-ui/core"
+
+import { theme } from "../src/App/theme"
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   viewport: { defaultViewport: "mobile2", disable: false },
 }
+
+export const decorators = [
+  (Story) => (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Story />
+    </ThemeProvider>
+  ),
+]
