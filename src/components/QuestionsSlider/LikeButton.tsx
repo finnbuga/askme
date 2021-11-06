579a7afeabc4a5980c72523d4c694fab1a7d1e42
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2021-11-06 11:10:59 +0000

    feat: style like, next & prev buttons

--- src/components/QuestionsSlider/LikeButton.tsx
+++ src/components/QuestionsSlider/LikeButton.tsx
@@ -1,17 +1,11 @@
 import React from "react"
-import { IconButton } from "@material-ui/core"
-import FavoriteIcon from "@material-ui/icons/Favorite"
-import FavoriteBorderIcon from "@material-ui/icons/FavoriteBorder"
+import { IconButton, SvgIcon } from "@material-ui/core"
 
 import { useDispatch, useSelector } from "store"
 import { notifyError } from "store/notificationsSlice"
 import { likeQuestion, unlikeQuestion } from "store/userSlice"
 
 import type { Question } from "api/questions"
-
-const largeFont = {
-  fontSize: "2.4rem",
-}
 
 const LikeButton: React.FC<{ questionId: Question["id"] }> = ({ questionId: id }) => {
   const user = useSelector((state) => state.user.user)
@@ -30,10 +24,19 @@ const LikeButton: React.FC<{ questionId: Question["id"] }> = ({ questionId: id }
   }
 
   return (
-    <IconButton onClick={handleLike} color={isLiked ? "error" : "default"}>
-      {isLiked ? <FavoriteIcon style={largeFont} /> : <FavoriteBorderIcon style={largeFont} />}
+    <IconButton onClick={handleLike}>
+      <HeartIcon filled={isLiked} />
     </IconButton>
   )
 }
+
+const HeartIcon: React.FC<{ filled?: boolean }> = ({ filled }) => (
+  <SvgIcon viewBox="0 0 33 32" color="primary" style={{ fontSize: 32 }}>
+    <svg fill={filled ? "currentColor" : "none"} strokeWidth={1.5} stroke="#137EC2">
+      <path d="M16.1753 27.805C13.2811 26.0239 10.5888 23.9275 8.14632 21.5536C6.42919 19.8439 5.12194 17.7597 4.32473 15.4605C2.89013 11.0004 4.56584 5.89444 9.25543 4.38336C11.7201 3.58991 14.4119 4.0434 16.4887 5.60197C18.5664 4.0453 21.2572 3.59197 23.722 4.38336C28.4116 5.89444 30.0994 11.0004 28.6648 15.4605C27.8676 17.7597 26.5603 19.8439 24.8432 21.5536C22.4008 23.9275 19.7084 26.0239 16.8142 27.805L16.5008 28L16.1753 27.805Z" />
+      <path d="M21.4797 9.40405C22.9001 9.85779 23.9094 11.133 24.0355 12.6334" />
+    </svg>
+  </SvgIcon>
+)
 
 export default LikeButton
--- src/components/QuestionsSlider/useNavigator.tsx
+++ src/components/QuestionsSlider/useNavigator.tsx
@@ -1,11 +1,5 @@
 import React, { useState } from "react"
-import { IconButton } from "@material-ui/core"
-import SkipNextIcon from "@material-ui/icons/SkipNext"
-import SkipPreviousIcon from "@material-ui/icons/SkipPrevious"
-
-const style = {
-  fontSize: "3rem",
-}
+import { IconButton, SvgIcon } from "@material-ui/core"
 
 const useNavigator = (size: number) => {
   const [current, setCurrent] = useState(0)
@@ -14,17 +8,35 @@ const useNavigator = (size: number) => {
 
   const NextButton: React.FC = () => (
     <IconButton onClick={goToNext} disabled={!goToNext}>
-      <SkipNextIcon style={style} />
+      <NextIcon />
     </IconButton>
   )
 
   const PrevButton: React.FC = () => (
     <IconButton onClick={goToPrev} disabled={!goToPrev}>
-      <SkipPreviousIcon style={style} />
+      <PreviousIcon />
     </IconButton>
   )
 
   return { current, NextButton, PrevButton }
 }
+
+const NextIcon: React.FC<{ filled?: boolean }> = ({ filled }) => (
+  <SvgIcon viewBox="0 0 41 40" color="primary" style={{ fontSize: 32 }}>
+    <svg fill="none">
+      <circle cx="20.2629" cy="20" r="20" fill="#DEDEDE" />
+      <path d="M17.8204 14.1667L23.6538 20L17.8204 25.8334" stroke="#137EC2" />
+    </svg>
+  </SvgIcon>
+)
+
+const PreviousIcon: React.FC<{ filled?: boolean }> = ({ filled }) => (
+  <SvgIcon viewBox="0 0 41 40" color="primary" style={{ fontSize: 32 }}>
+    <svg fill="none">
+      <circle cx="20.2629" cy="20" r="20" fill="#DEDEDE" />
+      <path d="M23.1796 25.8333L17.3462 20L23.1796 14.1666" stroke="#137EC2" />
+    </svg>
+  </SvgIcon>
+)
 
 export default useNavigator
