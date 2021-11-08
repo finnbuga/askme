46fbdba8b43d86b59dd1d6c14beb15c270f2a478
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2021-11-08 22:08:54 +0000

    refactor PageWrapper

--- src/App/PageWrapper/PageWrapper.tsx
+++ src/App/PageWrapper/PageWrapper.tsx
@@ -3,14 +3,6 @@ import { Box, Container, Theme } from "@material-ui/core"
 
 import backgroundImage from "./bottom-background.png"
 
-const wrapperStyle = {
-  minHeight: "100vh",
-  background: (theme: Theme) => ({
-    xs: theme.palette.grey[200],
-    sm: `url(${backgroundImage}) no-repeat bottom`,
-  }),
-} as const
-
 const PageWrapper: React.FC = ({ children }) => (
   <Box sx={wrapperStyle}>
     <Container maxWidth="sm" disableGutters>
@@ -18,5 +10,13 @@ const PageWrapper: React.FC = ({ children }) => (
     </Container>
   </Box>
 )
+
+const wrapperStyle = {
+  minHeight: "100vh",
+  background: (theme: Theme) => ({
+    xs: theme.palette.grey[200],
+    sm: `url(${backgroundImage}) no-repeat bottom`,
+  }),
+} as const
 
 export default PageWrapper
