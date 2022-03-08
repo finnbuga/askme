820355054d3120381b3b6dd73f95f914e37673be
Finn Buga <7334553+finnbuga@users.noreply.github.com>
2022-03-08 03:57:22 +0000

    fix import and remove inexistant prop

--- src/pages/MyQuestionsPage/AddQuestion.test.tsx
+++ src/pages/MyQuestionsPage/AddQuestion.test.tsx
@@ -1,14 +1,14 @@
 import React from "react"
 import { render, screen } from "@testing-library/react"
 
-import Question from "api/interfaces/Question"
+import { Question } from "api/questions"
 import AddQuestion from "./AddQuestion"
 
 const mockOnAdd = (question: Omit<Question, "id" | "userId">) => Promise.resolve()
 
 describe("AddQuestion", () => {
   test("renders the right CTA", () => {
-    render(<AddQuestion onAdd={mockOnAdd} />)
+    render(<AddQuestion />)
     const button = screen.getByText("Add question")
     expect(button).toBeInTheDocument()
   })
