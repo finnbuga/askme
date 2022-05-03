e844e9fafb785a77ae66063b921bb51fcb2b9edb
Finn Buga <7334553+finnbuga@users.noreply.github.com>
2022-05-03 21:50:41 +0100

    build: upgrade to React 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,21 +1,22 @@
 import React from "react"
-import ReactDOM from "react-dom"
+import { createRoot } from "react-dom/client"
 import { Provider } from "react-redux"
 import { BrowserRouter } from "react-router-dom"
 
 import store from "store"
 import App from "App"
 import reportWebVitals from "./reportWebVitals"
+const container = document.getElementById("root")
+const root = createRoot(container!)
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
     <BrowserRouter>
       <Provider store={store}>
         <App />
       </Provider>
     </BrowserRouter>
-  </React.StrictMode>,
-  document.getElementById("root")
+  </React.StrictMode>
 )
 
 // If you want to start measuring performance in your app, pass a function
