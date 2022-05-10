8c71f3a04a7cf91875d5bfeea17ad5e2b7f88559
Finn Buga <7334553+finnbuga@users.noreply.github.com>
2022-05-10 15:54:22 +0100

    feat: display logo in the center

--- src/App/Header/Logo.tsx
+++ src/App/Header/Logo.tsx
@@ -1,25 +1,11 @@
 import { Link } from "react-router-dom"
-import { Typography } from "@mui/material"
 
-const style = {
-  expand: {
-    flexGrow: 1,
-  },
-  logo: {
-    color: "inherit",
-    textDecoration: "none",
-  },
-}
+import logo from "./logo.png"
 
-function Logo() {
-  return (
-    <Typography variant="h6" style={style.expand}>
-      <Link to="/" style={style.logo}>
-        {/* <img width={30} src={logo} alt="Logo" />  */}
-        {/* Ask me! */}
-      </Link>
-    </Typography>
-  )
-}
+const Logo: React.FC = () => (
+  <Link to="/" style={{ color: "inherit", textDecoration: "none", flexGrow: 1 }}>
+    <img width={30} src={logo} alt="Logo" style={{ display: "block", margin: "0 auto" }} />
+  </Link>
+)
 
 export default Logo
