370618f5945f76aa5e56c224599e2b8ebfc147c4
Finn Buga <7334553+finnbuga@users.noreply.github.com>
2022-05-11 00:33:00 +0100

    fix: center logo

--- src/App/Header/UserMenu.tsx
+++ src/App/Header/UserMenu.tsx
@@ -12,8 +12,9 @@ function UserMenu() {
   const { user, isAuthenticating } = useSelector((state) => state.user)
 
   return (
-    // set a fixed width do avoid the header resizing on auth user response
-    <div style={{ width: 100, display: "flex", flexDirection: "row-reverse" }}>
+    // Set a fixed width do avoid the header resizing on auth user response
+    // Set it to 40 in order to match the width of the menu icon on the left. This will help center the logo.
+    <div style={{ width: 40, display: "flex", flexDirection: "row-reverse" }}>
       {isAuthenticating ? null : user ? <AuthMenu /> : <LoginButton />}
     </div>
   )
