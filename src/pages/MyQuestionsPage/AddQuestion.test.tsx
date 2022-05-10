a7be792848e17cb9570907db7d79db7b2702fd00
Finn Buga <7334553+finnbuga@users.noreply.github.com>
2022-05-10 22:47:33 +0100

    fix tests

--- src/pages/MyQuestionsPage/AddQuestion.test.tsx
+++ src/pages/MyQuestionsPage/AddQuestion.test.tsx
@@ -1,10 +1,17 @@
+import { Provider } from "react-redux"
+import { configureStore } from "@reduxjs/toolkit"
 import { render, screen } from "@testing-library/react"
 
 import AddQuestion from "./AddQuestion"
 
+const store = configureStore({ reducer: {} })
+const ReduxProvider: React.FC<React.PropsWithChildren<{}>> = ({ children }) => (
+  <Provider store={store}>{children}</Provider>
+)
+
 describe("AddQuestion", () => {
   test("renders the right CTA", () => {
-    render(<AddQuestion />)
+    render(<AddQuestion />, { wrapper: ReduxProvider })
     const button = screen.getByText("Add question")
     expect(button).toBeInTheDocument()
   })
