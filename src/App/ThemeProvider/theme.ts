6a240039d5ebb7b9e3ef544bea4e3d95603c31cf
Finn Buga <7334553+finnbuga@users.noreply.github.com>
2022-05-13 10:29:48 +0100

    refactor mui theme variables

--- src/App/ThemeProvider/theme.ts
+++ src/App/ThemeProvider/theme.ts
@@ -14,33 +14,33 @@ const palette = {
   text: {
     primary: "#525252",
   },
-}
-
-const typography = {
-  h1: {
-    color: palette.primary.main,
-    fontSize: "1.36rem",
-    lineHeight: 1.36,
-    marginTop: 0,
-    textAlign: "center",
-  },
-  h2: {
-    color: palette.primary.main,
-    fontSize: "1.25rem",
-    lineHeight: 1.65,
-  },
-  fontFamily: "Lato, sans-serif",
 } as const
 
 export const theme = createTheme({
-  components: {
-    MuiCssBaseline: {
-      styleOverrides: {
-        ...typography,
-      },
+  palette,
+  spacing: 8,
+  typography: {
+    h1: {
+      color: palette.primary.main,
+      fontSize: "1.36rem",
+      lineHeight: 1.36,
+      marginTop: 0,
+      textAlign: "center",
+    },
+    h2: {
+      color: palette.primary.main,
+      fontSize: "1.25rem",
+      lineHeight: 1.65,
+    },
+    fontFamily: "Lato, sans-serif",
+  },
+})
+
+theme.components = {
+  MuiCssBaseline: {
+    styleOverrides: {
+      ...theme.typography,
+      body: theme.typography.body1,
     },
   },
-  palette,
-  typography,
-  spacing: 8,
-})
+}
