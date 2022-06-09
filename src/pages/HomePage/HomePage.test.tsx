46c0922aa0889b7e73d10a7cf5b408a20d7b0239
Finn Buga <7334553+finnbuga@users.noreply.github.com>
2022-06-09 22:53:13 +0100

    refactor test to use expect()

--- src/pages/HomePage/HomePage.test.tsx
+++ src/pages/HomePage/HomePage.test.tsx
@@ -5,8 +5,10 @@ import HomePage from "./HomePage"
 describe("HomePage", () => {
   test("displays intro text", () => {
     render(<HomePage />)
-    screen.getByText(
-      "Spark insightful conversations and get you know yourself and your friends better"
-    )
+    expect(
+      screen.getByText(
+        "Spark insightful conversations and get you know yourself and your friends better"
+      )
+    ).toBeInTheDocument()
   })
 })
