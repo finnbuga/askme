57610352b33e750719e7e6342abd715906ce64a5
Finn Buga <7334553+finnbuga@users.noreply.github.com>
2022-06-20 12:02:27 +0300

    fix: add QueryClientProvider to test

--- src/pages/HomePage/HomePage.test.tsx
+++ src/pages/HomePage/HomePage.test.tsx
@@ -1,10 +1,18 @@
 import { render, screen } from "@testing-library/react"
+import { QueryClient, QueryClientProvider } from "react-query"
+import type { ReactNode } from "react"
 
 import HomePage from "./HomePage"
 
+const queryClient = new QueryClient()
+
+const Provider: React.FC<{ children: ReactNode }> = ({ children }) => (
+  <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+)
+
 describe("HomePage", () => {
   test("displays intro text", () => {
-    render(<HomePage />)
+    render(<HomePage />, { wrapper: Provider })
     expect(
       screen.getByText(
         "Spark insightful conversations and get you know yourself and your friends better"
