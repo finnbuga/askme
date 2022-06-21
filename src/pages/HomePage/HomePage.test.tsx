dc025ab4b5c3981d5616b48e76c4789e2864de4f
Finn Buga <7334553+finnbuga@users.noreply.github.com>
2022-06-21 18:20:06 +0300

    fix test: mock getQuestions()

--- src/pages/HomePage/HomePage.test.tsx
+++ src/pages/HomePage/HomePage.test.tsx
@@ -1,22 +1,37 @@
-import { render, screen } from "@testing-library/react"
+import { render, screen, waitFor } from "@testing-library/react"
 import { QueryClient, QueryClientProvider } from "react-query"
 import type { ReactNode } from "react"
 
+import { getQuestions } from "api/questions"
+
 import HomePage from "./HomePage"
 
-const queryClient = new QueryClient()
+jest.mock("api/questions")
+const getQuestionsMock = getQuestions as jest.MockedFunction<typeof getQuestions>
 
+const queryClient = new QueryClient()
 const Provider: React.FC<{ children: ReactNode }> = ({ children }) => (
   <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
 )
 
 describe("HomePage", () => {
-  test("displays intro text", () => {
+  test("displays intro text", async () => {
     render(<HomePage />, { wrapper: Provider })
-    expect(
-      screen.getByText(
+    getQuestionsMock.mockResolvedValueOnce([])
+
+    expect(getQuestionsMock).toHaveBeenCalledTimes(1)
+
+    await waitFor(() =>
+      expect(
+        screen.getByText(
+          "Spark insightful conversations and get you know yourself and your friends better"
+        )
+      ).toHaveTextContent(
         "Spark insightful conversations and get you know yourself and your friends better"
       )
-    ).toBeInTheDocument()
+    )
+    // await screen.findByText(
+    //   "Spark insightful conversations and get you know yourself and your friends better"
+    // )
   })
 })
