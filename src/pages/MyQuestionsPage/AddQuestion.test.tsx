671bf2f715a397c48ae4b79b1aab67697ff1ed6e
Finn Buga <7334553+finnbuga@users.noreply.github.com>
2022-06-23 22:57:19 +0300

    test AddQuestion properly

--- src/pages/MyQuestionsPage/AddQuestion.test.tsx
+++ src/pages/MyQuestionsPage/AddQuestion.test.tsx
@@ -1,11 +1,31 @@
 import { render, screen } from "testing"
+import { type, click } from "@testing-library/user-event"
+
+import * as api from "api/questions"
 
 import AddQuestion from "./AddQuestion"
 
+jest.mock("api/questions")
+
+const addQuestionMock = api.addQuestion as jest.MockedFunction<typeof api.addQuestion>
+
+const text = "Example question"
+
+const question = {
+  text,
+  timestamp: Date.now(),
+  userId: undefined,
+  id: "id",
+}
+
 describe("AddQuestion", () => {
   test("renders the right CTA", () => {
+    addQuestionMock.mockResolvedValueOnce(question)
     render(<AddQuestion />)
+
+    const nameInput = screen.getByText(/text/i)
+    type(nameInput, text)
     const button = screen.getByText("Add question")
-    expect(button).toBeInTheDocument()
+    click(button)
   })
 })
