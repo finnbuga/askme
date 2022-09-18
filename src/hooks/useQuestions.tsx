029626ad84b75fc9a3848ae17578a2860fadb797
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2022-09-18 20:47:10 +0100

    refactor: rename type Filter to FilterFn

--- src/hooks/useQuestions.tsx
+++ src/hooks/useQuestions.tsx
@@ -3,9 +3,9 @@ import { useQuery } from "react-query"
 import { getQuestions } from "api/questions"
 import type { Question } from "api/questions"
 
-export type Filter = (question: Question) => boolean
+export type FilterFn = (question: Question) => boolean
 
-export const useQuestions = ({ filter }: { filter?: Filter }) => {
+export const useQuestions = ({ filter }: { filter?: FilterFn }) => {
   const { data, isLoading, error } = useQuery("questions", getQuestions)
   let questions = data ?? []
   if (filter) {
