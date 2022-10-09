e54bd56b0118d9ba6f50fdf78031b8d704063b91
Finn Buga <7334553+finnbuga@users.noreply.github.com>
2022-10-09 04:10:05 +0300

    refactor: use H1 instead of h1

--- src/pages/MyFavouritesPage.tsx
+++ src/pages/MyFavouritesPage.tsx
@@ -1,8 +1,9 @@
-import { Box, Alert } from "@mui/material"
+import { Alert } from "@mui/material"
 
 import type { Question } from "api/questions"
 import { useSelector } from "store"
 import QuestionsSlider from "components/QuestionsSlider"
+import { H1 } from "components/ui/H1"
 
 const MyFavouritesPage: React.FC = () => {
   const { user, isAuthenticating } = useSelector((state) => state.user)
@@ -10,15 +11,13 @@ const MyFavouritesPage: React.FC = () => {
 
   return (
     <>
-      <h1>My Favourites</h1>
+      <H1 mb={10}>My Favourites</H1>
 
-      <Box sx={{ mt: 10 }}>
-        {isAuthenticating ? null : !user ? (
-          <Alert severity="info">In order to add your favourites please login</Alert>
-        ) : (
-          <QuestionsSlider filter={isMyFavourite} />
-        )}
-      </Box>
+      {isAuthenticating ? null : !user ? (
+        <Alert severity="info">In order to add your favourites please login</Alert>
+      ) : (
+        <QuestionsSlider filter={isMyFavourite} />
+      )}
     </>
   )
 }
