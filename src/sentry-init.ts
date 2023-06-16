b5b16a1411d985cec7d0d5019fd7c098d06f265d
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2023-06-16 03:42:54 +0300

    feat: turn off Sentry debug

--- src/sentry-init.ts
+++ src/sentry-init.ts
@@ -5,7 +5,7 @@ Sentry.init({
   dsn: "https://275c263afc0c425a886ab9adfcb233e5@o4504705907425280.ingest.sentry.io/4504705910112256",
   integrations: [new BrowserTracing()],
   environment: "production",
-  debug: true,
+  debug: false,
 
   // Set tracesSampleRate to 1.0 to capture 100%
   // of transactions for performance monitoring.
