dae68b2bab75f1556b03ad26c4f7ee6974914e55
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2023-08-15 23:46:46 +0100

    build: fix ESLint

--- vite.config.ts
+++ vite.config.ts
@@ -14,6 +14,9 @@ export default defineConfig({
     checker({
       overlay: { initialIsOpen: false },
       typescript: true,
+      eslint: {
+        lintCommand: 'eslint "./src/**/*.{ts,tsx}"',
+      },
     }),
     viteTsconfigPaths(),
     svgrPlugin(),
