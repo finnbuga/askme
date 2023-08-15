66cdf8ebba20b801daa8b9a09a60b380cb8ff607
Florin Buga <7334553+finnbuga@users.noreply.github.com>
2023-08-15 23:22:42 +0100

    build: replace CRA with Vite

---
+++ vite.config.ts
@@ -0,0 +1,34 @@
+import { resolve } from 'path';
+
+import { defineConfig, Plugin } from 'vite';
+import react from '@vitejs/plugin-react';
+import viteTsconfigPaths from 'vite-tsconfig-paths';
+import svgrPlugin from 'vite-plugin-svgr';
+import checker from 'vite-plugin-checker';
+import handlebars from 'vite-plugin-handlebars';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    checker({
+      overlay: { initialIsOpen: false },
+      typescript: true,
+    }),
+    viteTsconfigPaths(),
+    svgrPlugin(),
+    handlebars({
+      partialDirectory: resolve(__dirname, 'src/partials'),
+    }) as Plugin,
+  ],
+  server: {
+    port: 3000,
+    proxy: {
+      '/api-server/': '...',
+      '/authorization/': '...',
+    },
+  },
+  build: {
+    outDir: 'build',
+  },
+});
